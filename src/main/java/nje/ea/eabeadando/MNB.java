167a2d2e81bbcc3ff57ad3568dbe8aa7c3f5be2a
Hoki Attila <hoki.attila@gmail.com>
2024-12-03 15:58:32 +0100

    Ported to JDK17

--- src/main/java/nje/ea/eabeadando/MNB.java
+++ src/main/java/nje/ea/eabeadando/MNB.java
@@ -20,7 +20,13 @@ public class MNB {
                 }
             }
 
-            ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarPath, currency, startDate, endDate);
+            // ProcessBuilder a JAR futtatására a szükséges JVM paraméterrel
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "java",
+                    "--add-opens", "java.base/java.lang=ALL-UNNAMED", // Hozzáadjuk a szükséges JVM paramétert
+                    "-jar", jarPath,
+                    currency, startDate, endDate
+            );
 
             // Parancs futtatása
             Process process = processBuilder.start();
@@ -30,22 +36,22 @@ public class MNB {
                 throw new RuntimeException("SOAP-MNB.jar futtatása sikertelen! Hibakód: " + exitCode);
             }
 
-            // 4. MNB.txt fájl megnyitása
+            // MNB.txt fájl megnyitása
             File file = new File("c:\\adatok\\MNB.txt");
             if (!file.exists()) {
                 throw new IOException("Nem található a letöltött MNB.txt fájl: " + file.getAbsolutePath());
             }
 
-            if(openResult) {
-                    // 5. c:\adatok mappa megnyitása
-                    if (Desktop.isDesktopSupported()) {
-                        Desktop.getDesktop().open(dataFolder);
-                    } else {
-                        throw new UnsupportedOperationException("A rendszer nem támogatja a mappanyitást");
-                    }
+            if (openResult) {
+                // c:\adatok mappa megnyitása
+                if (Desktop.isDesktopSupported()) {
+                    Desktop.getDesktop().open(dataFolder);
+                } else {
+                    throw new UnsupportedOperationException("A rendszer nem támogatja a mappanyitást");
+                }
 
-                    // Alapértelmezett rendszerprogrammal nyitja meg az MNB.txt-t
-                    Desktop.getDesktop().open(file);
+                // Alapértelmezett rendszerprogrammal nyitja meg az MNB.txt-t
+                Desktop.getDesktop().open(file);
             }
         } catch (Exception e) {
             // Hiba esetén felugró ablak jelenik meg
--- src/main/java/nje/ea/eabeadando/MainApp.java
+++ src/main/java/nje/ea/eabeadando/MainApp.java
@@ -10,6 +10,8 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            System.out.println("Java Runtime Version: " + System.getProperty("java.version"));
+            System.out.println("Java Runtime Vendor: " + System.getProperty("java.vendor"));
             FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/nje/ea/eabeadando/menu-bar.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 900, 600);
             stage.setTitle("JavaFX CRUD Alkalmazás");
